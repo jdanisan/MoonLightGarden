41232b20f2b38d92933d1906e585d5879aab4a5d
jdanisan <jdanielsagitario@gmail.com>
2025-05-11 15:49:10 +0200

    Creación MainActivity

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.moonlightgarden"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.moonlightgarden"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation ("com.google.android.material:material:1.9.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.firebase:firebase-auth:22.1.2")
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+}
---
+++ app/src/main/java/com/example/moonlightgarden/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.moonlightgarden;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import androidx.activity.ComponentActivity;
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends ComponentActivity {
+
+    ViewFlipper v_flipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableEdgeToEdge();
+        setContentView(R.layout.activity_main);
+        //Debido a un fallo a solucionar hoy, actualmente 09/05/2025 las imagenes del banner van a estar en res/drawable/
+        int images[] = {R.drawable.anuncion1,R.drawable.anuncio2,R.drawable.anuncio3,R.drawable.anuncio4,R.drawable.anuncio5};
+
+        v_flipper= findViewById(R.id.v_flipper);
+
+        for(int image: images){
+            flipperImages(image);
+        }
+
+
+        FirebaseApp.initializeApp(this);
+    }
+
+    private void enableEdgeToEdge() {
+        // Implement the edge-to-edge functionality if needed
+    }
+
+    public void flipperImages(int images){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(images);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(3000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(this, android.R.anim.slide_out_right);
+        v_flipper.setOutAnimation(this,android.R.anim.slide_in_left);
+    }
+}
